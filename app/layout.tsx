a1f0d03b350feda4dcf171923318faa93063ac68
shantmelikyan <shantmelikyan@gmail.com>
2023-05-31 00:04:59 -0700

    images, font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Advent_Pro } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Advent_Pro({
+  weight: "600",
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
 
 export const metadata = {
   title: 'Shant Photography',
@@ -14,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body >{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,14 @@ import { Tab } from "@headlessui/react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Masonry from "react-masonry-css";
+import classNames from "classnames";
+
+import ocean1 from 'public/ocean-1.jpg'
+import ocean2 from 'public/ocean-2.jpg'
+import ocean3 from 'public/ocean-3.jpg'
+import ocean4 from 'public/ocean-4.jpg'
+import ocean5 from 'public/ocean-5.jpg'
 
 const tabs = [
   {
@@ -18,32 +26,35 @@ const tabs = [
     display: "Forests",
   },
 ];
-
+const images = [];
 export default function Home() {
   return (
-    <div className="flex flex-col h-full bg-[url('/bg-image.jpg')] bg-top bg-cover">
-      <header className="flex justify-between items-center h-[90px] px-6">
+    <div className="h-full bg-[url('/bg-image.jpg')] bg-top bg-cover overflow-auto">
+      <header className="fixed top-0 w-full z-10 flex justify-between items-center h-[90px] px-6">
         {/* <div className="rounded-3xl bg-white text-black px-3 py-2">Menu</div> */}
         <div className="rounded-3xl h-10 overflow-hidden">
           <img className=" w-[84px] px-5 bg-white" src="/logo.png" alt="" />
         </div>
         <Link
           href="#home"
-          className="rounded-3xl bg-white text-black font-bold px-3 py-2 hover:bg-opacity-80"
+          className="uppercase rounded-3xl bg-white text-black text-md px-3 py-2 hover:bg-opacity-80"
         >
           Contact
         </Link>
       </header>
 
-      <main className="grow">
+      <main className="pt-[110px]">
         <div className="flex flex-col items-center h-full">
           <Tab.Group>
-            <Tab.List className="flex items-center gap-12">
+            <Tab.List className="flex items-center gap-12 ">
               {tabs.map((tab) => (
                 <Tab key={tab.key}>
                   {({ selected }) => (
                     <span
-                      className={selected ? "text-white" : "text-stone-600"}
+                      className={classNames(
+                        "uppercase text-lg",
+                        selected ? "text-white" : "text-stone-600"
+                      )}
                     >
                       {tab.display}
                     </span>
@@ -51,8 +62,20 @@ export default function Home() {
                 </Tab>
               ))}
             </Tab.List>
-            <Tab.Panels className="h-full bg-stone-900 bg-opacity-80 max-w-[900px] w-full p-2 sm:p-4 my-6">
-              <Tab.Panel className="">All Photos</Tab.Panel>
+            <Tab.Panels className="h-full max-w-[900px] w-full p-2 sm:p-4 my-6">
+              <Tab.Panel className="">
+                <Masonry
+                  breakpointCols={2}
+                  className="flex gap-4"
+                  columnClassName="my-masonry-grid_column"
+                >
+                  <Image src={ocean1} alt="ocean-1" className="my-4" />
+                  <Image src={ocean2} alt="ocean-2" className="my-4" />
+                  <Image src={ocean3} alt="ocean-3" className="my-4" />
+                  <Image src={ocean4} alt="ocean-4" className="my-4" />
+                  <Image src={ocean5} alt="ocean-5" className="my-4" />
+                </Masonry>
+              </Tab.Panel>
               <Tab.Panel>Landscape pics</Tab.Panel>
               <Tab.Panel>Ocean pics</Tab.Panel>
             </Tab.Panels>
@@ -60,7 +83,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className="h-[60px] flex  justify-center items-center">
+      <footer className="h-[90px] uppercase text-lg flex justify-center items-center">
         <p>placeholder footer</p>
       </footer>
     </div>
